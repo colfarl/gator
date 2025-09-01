663b0479249067751c233a64e02a6ca2826c5bd4
Colin Farley <colinfarley0423@gmail.com>
2025-09-01 14:42:42 -0400

    added config package

---
+++ internal/config/config.go
@@ -0,0 +1,82 @@
+//Package config to manage credentials and user
+package config
+
+import (
+	"os"
+	"encoding/json"
+)
+
+// ======== Helpers ======== 
+const configFileName = ".gatorconfig.json" 
+
+func getConfigPath() (string, error) {
+	result, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+	return result + "/" + configFileName, nil
+}
+
+func write(cfg Config) error {
+
+	configFilePath, err := getConfigPath()
+	if err != nil {
+		return err
+	}
+
+	jsonData, err := json.MarshalIndent(cfg, "", "  ")
+	if err != nil {
+		return err
+	}
+	
+	err = os.WriteFile(configFilePath, jsonData, 0644)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+// ======== Exports ======== 
+
+type Config struct {
+	DBURL           string `json:"db_url"`
+	CurrentUserName string `json:"current_user_name"`
+}
+
+
+func Read() (Config, error) {
+
+	configFilePath, err := getConfigPath() 
+
+	if err != nil {
+		return Config{}, err
+	}
+	
+	body, err := os.ReadFile(configFilePath) 
+	if err != nil {
+		return Config{}, err
+	}
+	
+	var config Config
+	err = json.Unmarshal(body, &config)
+	if err != nil {
+		return Config{}, err
+	}
+
+	return config, nil
+}
+
+
+func SetUser(newUser string, cfg Config) error {		
+
+	cfg.CurrentUserName = newUser
+
+	err := write(cfg)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+
--- main.go
+++ main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"github.com/colfarl/gator/internal/config"
 )
 
 func main() {
-	fmt.Println("Hello world")
+
+	cfg, err :=  config.Read()
+	if err != nil {
+		fmt.Println(err)
+		return 
+	}
+
+	err = config.SetUser("colin", cfg)
+	if err != nil {
+		fmt.Println(err)
+		return 
+	}
+	
+	
+	cfg, err = config.Read()
+	if err != nil {
+		fmt.Println(err)
+		return 
+	}
+	
+	fmt.Println(cfg)
 }
